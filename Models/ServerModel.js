53fed0a004428016d399b1722a0d84e3f0554319
Dilili <miniaoe@126.com>
2021-06-20 01:12:09 +0800

    fix server model & add bandwidth options

--- Models/ServerModel.js
+++ Models/ServerModel.js
@@ -7,6 +7,7 @@ const ServerModel = sequelize.define("Server", {
   name: { type: DataTypes.STRING, allowNull: false, comment: "服务器名字" },
   local: { type: DataTypes.STRING, allowNull: false, comment: "地区" },
   ip_addr: { type: DataTypes.STRING, allowNull: false, comment: "ip地址" },
+  bandwidth: { type: DataTypes.INTEGER, allowNull: false, comment: "带宽" },
   status: { type: DataTypes.TINYINT, defaultValue: 0, comment: "0离线 1在线" },
 });
 
